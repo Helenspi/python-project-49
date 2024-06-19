ec95f5edb1a859330457d633c39779168e72dff3
Elena Spivakova <helenspivakova@gmail.com>
2024-06-19 16:16:45 +0300

    repair brain_prime_game

--- brain_games/games/brain_prime_game.py
+++ brain_games/games/brain_prime_game.py
@@ -6,13 +6,14 @@ TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def problem():
     number = random.randint(1, 200)
-    if number % 2 == 0:
-        number = number + 1
-
+    k = 0
     if number == 1:
         result = 'no'
-    for i in range(2, (number // 2) + 1):
+    for i in range(2, number // 2 + 1):
         if number % i == 0:
-            result = 'no'
-    result = 'yes'
+            k += 1
+    if k <= 0:
+        result = 'yes'
+    else:
+        result = 'no'
     return number, result
