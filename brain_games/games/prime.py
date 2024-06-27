c048c7836ff55e729df5395fae20acfd1d2a73d1
Elena Spivakova <helenspivakova@gmail.com>
2024-06-28 01:30:40 +0300

    modified prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -8,13 +8,13 @@ def is_prime(num):
     """
     Function determines whether a number is
     prime or not.
-    **Arg**: number
-    **Returns**: boolean values
+    Arg: number
+    Returns: boolean values
     """
-    if num < 2 or num % 2 == 0:
-        return False
     if num == 2 or num == 3:
         return True
+    elif num < 2 or num % 2 == 0:
+        return False
     for i in range(3, num // 2 + 1):
         return False if num % i == 0 else True
 
@@ -23,8 +23,8 @@ def create_task():
     """
     Function generates random number and
     applies is_prime function to it.
-    **Arg**: no
-    **Returns**: number, yes/no answer
+    Arg: no
+    Returns: number, yes/no answer
     """
     number = random.randint(1, 200)
     if is_prime(number) is True:
